ece11ad7807e7aa7dca0482cb17bb0bc5c93498a
Đặng Quang Anh <42268330+Anh1223@users.noreply.github.com>
2018-10-25 19:11:15 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var start = document.querySelector(".start")
-.addEventListener("click", () =>{
+// var start = document.querySelector(".start")
+// .addEventListener("click", () =>{
+//     document.body.innerHTML = '';
     
- })
+// })
+
+//  function changeImage() {    
+//     var image = document.getElementById('myImage');   
+// if (image.src.match("bulbon")) {        image.src = "part 3.png";    }
+// else {        image.src = "part 3.png";    }
+// }
