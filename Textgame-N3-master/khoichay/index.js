1b7f3515dadf9203a1169782dcee35c4c7aa2178
Đặng Quang Anh <42268330+Anh1223@users.noreply.github.com>
2018-10-28 02:55:31 +0700

    Add files via upload

---
+++ Textgame-N3-master/khoichay/index.js
@@ -0,0 +1,10 @@
+var start = document.querySelector("")
+.addEventListener("click", () =>{
+    
+})
+
+//  function changeImage() {    
+//     var image = document.getElementById('myImage');   
+// if (image.src.match("bulbon")) {        image.src = "part 3.png";    }
+// else {        image.src = "part 3.png";    }
+// }
